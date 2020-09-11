da7fee5e1c6ff6e61a7034abde75aca4218c3b3b
Isami Odagiri <gurentosouen@gmail.com>
2020-09-12 01:02:52 +0900

    初期設定　StoryBoard 削除

---
+++ GitHubAPIForRxSwift/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  GitHubAPIForRxSwift
+//
+//  Created by Isami Odagiri on 2020/09/12.
+//  Copyright © 2020 Isami Odagiri. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+    }
+
+
+}
+
---
+++ GitHubAPIForRxSwift/SceneDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  SceneDelegate.swift
+//  GitHubAPIForRxSwift
+//
+//  Created by Isami Odagiri on 2020/09/12.
+//  Copyright © 2020 Isami Odagiri. All rights reserved.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+}
+
